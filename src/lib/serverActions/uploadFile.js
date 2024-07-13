faaa6d1420286041a624771d00ed86da45401c6b
h4lo343 <92515186+h4lo343@users.noreply.github.com>
2024-07-13 14:26:14 +1000

    fix/promisify-the-read-stream

--- src/lib/serverActions/uploadFile.js
+++ src/lib/serverActions/uploadFile.js
@@ -4,6 +4,7 @@ import fs from "fs";
 import { writeFile } from "fs/promises";
 import csvParser from "csv-parser";
 import { Readable } from "stream";
+import { resolve } from "path";
 
 export async function uploadFile(formData) {
   const file = formData.get("file");
@@ -12,11 +13,17 @@ export async function uploadFile(formData) {
   const stream = Readable.from(buffer);
 
   const result = [];
-  stream.pipe(csvParser()).on("data", (data) => {
-    result.push(data);
+  await new Promise((res, rej) => {
+    stream
+      .pipe(csvParser())
+      .on("data", (data) => {
+        result.push(data);
+      })
+      .on("finish", () => res());
   });
-
-  await writeFile("./a.csv", buffer);
+  console.log("-------------------------------");
+  console.log(result);
+  console.log("-------------------------------");
   const { data, error } = await supabase.from("customers").insert(result);
   if (error) {
     return { error: "Format not correct" };
